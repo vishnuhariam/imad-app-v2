9f0e141df83b451d7c5f7bffa73b13faab3eb0c3
vishnuhariam <vishnuhariam@gmail.com>
2017-03-13 20:58:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('article-one',function (req, res){
+app.get('/article-one',function (req, res){
    res.send('Hey Dude. Hi!!'); 
 });
 app.get('/', function (req, res) {
